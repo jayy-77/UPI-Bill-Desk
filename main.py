ff6b7d54b7c83370cb8329bd01b2e3bccc9387ef
jayy-77 <1427jay@gmail.com>
2023-02-26 13:40:07 +0530

    v1.5 => Payment success failure validation added

--- main.py
+++ main.py
@@ -14,10 +14,10 @@ sheet = client.open("upi_bill_desk_data").sheet1
 data_table = PrettyTable(['Index','Name','Price','Quantity'])
 procduct_data_dict = sheet.get_all_records()
 local_product_data = {
-    'index':[],
-    'product':[],
-    'price':[],
-    'quantity':[]
+    'index': [],
+    'product': [],
+    'price': [],
+    'quantity': []
 }
 
 for i in procduct_data_dict:
@@ -28,19 +28,26 @@ for i in procduct_data_dict:
     data_table.add_row([i['index'],i['product'],[i['price']],i['quantity']])
 print(data_table)
 
-choice = None
 bill_amount = 0
 
 while True:
     product_choice = input("Index <space> Quantity: ")
-    iq_lst = product_choice.split()
     if product_choice != 'q':
+        iq_lst = product_choice.split()
         if local_product_data['quantity'][int(iq_lst[0])-1] - int(iq_lst[1]) >= 0:
             bill_amount += local_product_data['price'][int(iq_lst[0])-1] * int(iq_lst[1])
-            local_product_data['quantity'][int(iq_lst[0])-1] -= int(iq_lst[1])
-            sheet.update_cell(int(iq_lst[0])+1, 4, local_product_data['quantity'][int(iq_lst[0])-1])
+            local_product_data['quantity'][int(iq_lst[0]) - 1] -= int(iq_lst[1])
         else:
             print("Insuffiecient stock")
     else:
-        Qr_operations.make_upi_qr(bill_amount)
-        bill_amount = 0
+        choice = input("Status ? Success | Failed (Y/N)")
+        if choice.lower() == 'y':
+            sheet.update_cell(int(iq_lst[0]) + 1, 4, local_product_data['quantity'][int(iq_lst[0]) - 1])
+            Qr_operations.make_upi_qr(bill_amount)
+            bill_amount = 0
+
+        else:
+            local_product_data['quantity'][int(iq_lst[0]) - 1] += int(iq_lst[1])
+            bill_amount = 0
+            continue
+        exec(open("main.py").read())

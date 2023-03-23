fb7c60ec6b845f80e00e547e2483a4ae3fd73933
jayy-77 <1427jay@gmail.com>
2023-03-24 00:17:45 +0530

    v1.6(Stable)=> Improved code reuseability and fixed bug.

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from prettytable import PrettyTable
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 from pprint import pprint
-
 import Qr_operations
 
+#Credentials for console cloud of google
 scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets',
 		"https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
 creds = ServiceAccountCredentials.from_json_keyfile_name("upibilldesk-daee7232a5ac.json", scope)
@@ -19,7 +19,6 @@ local_product_data = {
     'price': [],
     'quantity': []
 }
-
 for i in procduct_data_dict:
     local_product_data['index'].append(i['index'])
     local_product_data['product'].append(i['product'])
@@ -29,25 +28,27 @@ for i in procduct_data_dict:
 print(data_table)
 
 bill_amount = 0
-
+l=[]
 while True:
     product_choice = input("Index <space> Quantity: ")
     if product_choice != 'q':
         iq_lst = product_choice.split()
         if local_product_data['quantity'][int(iq_lst[0])-1] - int(iq_lst[1]) >= 0:
             bill_amount += local_product_data['price'][int(iq_lst[0])-1] * int(iq_lst[1])
-            local_product_data['quantity'][int(iq_lst[0]) - 1] -= int(iq_lst[1])
+            l += [[int(iq_lst[0])-1,int(iq_lst[1])]]
         else:
             print("Insuffiecient stock")
     else:
+        Qr_operations.make_upi_qr(bill_amount)
         choice = input("Status ? Success | Failed (Y/N)")
         if choice.lower() == 'y':
-            sheet.update_cell(int(iq_lst[0]) + 1, 4, local_product_data['quantity'][int(iq_lst[0]) - 1])
-            Qr_operations.make_upi_qr(bill_amount)
+            for i in l:
+                local_product_data['quantity'][i[0]] -= i[1]
+                sheet.update_cell(i[0]+2, 4, local_product_data['quantity'][i[0]])
             bill_amount = 0
-
+            l = []
+            break
         else:
-            local_product_data['quantity'][int(iq_lst[0]) - 1] += int(iq_lst[1])
+            l = []
             bill_amount = 0
-            continue
-        exec(open("main.py").read())
+exec(open("main.py").read())

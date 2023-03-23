606d76cbf8be17c196289969c5ea152cb56c65e2
jayy-77 <1427jay@gmail.com>
2023-03-24 02:15:58 +0530

    v1.6(Stable,comments)=> Improved code reuseability and fixed bug.

--- main.py
+++ main.py
@@ -10,15 +10,17 @@ scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/au
 creds = ServiceAccountCredentials.from_json_keyfile_name("upibilldesk-daee7232a5ac.json", scope)
 client = gspread.authorize(creds)
 
-sheet = client.open("upi_bill_desk_data").sheet1
-data_table = PrettyTable(['Index','Name','Price','Quantity'])
-procduct_data_dict = sheet.get_all_records()
+#Accessing sheet1 from upi_bill_desk_data sheet
+sheet = client.open("upi_bill_desk_data").sheet1 #sheet object for sheet1
+data_table = PrettyTable(['Index','Name','Price','Quantity']) # Table head
+procduct_data_dict = sheet.get_all_records() #Storing data in dictionary from sheet object.
 local_product_data = {
     'index': [],
     'product': [],
     'price': [],
     'quantity': []
 }
+#Printing sheet data in table format.
 for i in procduct_data_dict:
     local_product_data['index'].append(i['index'])
     local_product_data['product'].append(i['product'])
@@ -29,19 +31,21 @@ print(data_table)
 
 bill_amount = 0
 l=[]
+#Infinite conditional loop
 while True:
     product_choice = input("Index <space> Quantity: ")
     if product_choice != 'q':
-        iq_lst = product_choice.split()
-        if local_product_data['quantity'][int(iq_lst[0])-1] - int(iq_lst[1]) >= 0:
-            bill_amount += local_product_data['price'][int(iq_lst[0])-1] * int(iq_lst[1])
-            l += [[int(iq_lst[0])-1,int(iq_lst[1])]]
+        iq_lst = product_choice.split() #Splitting input for index and quantity
+        if local_product_data['quantity'][int(iq_lst[0])-1] - int(iq_lst[1]) >= 0: #Checking if quantity is available or not.
+            bill_amount += local_product_data['price'][int(iq_lst[0])-1] * int(iq_lst[1]) #Totalling bill.
+            l += [[int(iq_lst[0])-1,int(iq_lst[1])]] #Making 2d list for storing index and quantity for multiple orders.
         else:
             print("Insuffiecient stock")
     else:
-        Qr_operations.make_upi_qr(bill_amount)
+        Qr_operations.make_upi_qr(bill_amount) #Call make_upi_qr from Qr_operations class
         choice = input("Status ? Success | Failed (Y/N)")
         if choice.lower() == 'y':
+            #Updating data on local dictionary and google sheet
             for i in l:
                 local_product_data['quantity'][i[0]] -= i[1]
                 sheet.update_cell(i[0]+2, 4, local_product_data['quantity'][i[0]])
@@ -49,6 +53,8 @@ while True:
             l = []
             break
         else:
+            #Resetting
             l = []
             bill_amount = 0
+#Executing script within script
 exec(open("main.py").read())
